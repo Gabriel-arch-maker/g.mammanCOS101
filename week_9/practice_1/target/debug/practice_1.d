dbd00b0a55bf2999b343c18774a2db1d7fce1c4e
g.mammanCOS101 <gmghorse@gmail.com>
2023-12-09 23:56:09 +0100

    (ninth)

---
+++ week_9/practice_1/target/debug/practice_1.d
@@ -0,0 +1,1 @@
+C:\Users\GABRIEL\Documents\g.mammanCOS101\week_9\practice_1\target\debug\practice_1.exe: C:\Users\GABRIEL\Documents\g.mammanCOS101\week_9\practice_1\src\main.rs
---
+++ week_9/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\GABRIEL\Documents\g.mammanCOS101\week_9\practice_2\target\debug\practice_2.exe: C:\Users\GABRIEL\Documents\g.mammanCOS101\week_9\practice_2\src\main.rs
